390484f132580bfd634fd3b9a6c3bae74d8d9145
Aatish Katuwal <aatishkatuwal777@gmail.com>
2025-12-15 10:06:39 -0600

    Delete Home Work Assignment 4 directory

--- Home Work Assignment 4/Home Work Assignment 4 (INSYS)/name.py
+++
@@ -1,8 +0,0 @@
-def main():
-    full_name = input("Enter full name: ")
-    name_list = full_name.split()
-
-    for i in range(len(name_list)):
-        print(name_list[i][0]+".", end="")
-
-main()
